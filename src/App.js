8ba72b15891ed12b987c32de8121f46ee3db2a8f
Rob van der Leek <robvanderleek@gmail.com>
2022-01-25 23:29:46 +0100

    Hello MUI 5 :wave:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import {Avatar} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Avatar src="http://assets.stickpng.com/images/5cb480cd5f1b6d3fbadece79.png" sx={{width: 64, height: 64}}/>
   );
 }
 
